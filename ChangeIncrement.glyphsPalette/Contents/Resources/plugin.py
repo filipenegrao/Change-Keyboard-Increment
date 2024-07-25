97e6b1106cffc94439b4adee9f570cebb05bdaf7
schriftgestalt <georg.seifert@mac.com>
2024-07-25 11:27:18 -0700

    use bindings and add keyboardHuge
    the userDefaults keys should not contain periods when used in UI bindings. So I replaced them with underscores

--- ChangeIncrement.glyphsPalette/Contents/Resources/plugin.py
+++ ChangeIncrement.glyphsPalette/Contents/Resources/plugin.py
@@ -23,21 +23,6 @@ class KeyboardIncrement(PalettePlugin):
 	# the view
 	dialog = objc.IBOutlet()
 
-	# keyboard increment changer UIs
-	keyboard_switch = objc.IBOutlet()
-	keyboard_txt_keys = objc.IBOutlet()
-	keyboard_txt_shift = objc.IBOutlet()
-
-	# metrics increment changer UIs
-	metrics_switch = objc.IBOutlet()
-	metrics_txt_keys = objc.IBOutlet()
-	metrics_txt_shift = objc.IBOutlet()
-
-	# kerning increment changer UIs
-	kerning_switch = objc.IBOutlet()
-	kerning_txt_keys = objc.IBOutlet()
-	kerning_txt_shift = objc.IBOutlet()
-
 	@objc.python_method
 	def settings(self):
 		self.name = Glyphs.localize({
@@ -56,116 +41,55 @@ class KeyboardIncrement(PalettePlugin):
 		else:
 			self.loadNib('IBdialogpre15', __file__)
 
-	@objc.python_method
-	def start(self):
-		Glyphs.registerDefault('com.filipenegrao.Increment.keyboard_small', 1)
-		Glyphs.registerDefault('com.filipenegrao.Increment.keyboard_big', 10)
+		Glyphs.registerDefault('com_filipenegrao_Increment_keyboard_small', 1)
+		Glyphs.registerDefault('com_filipenegrao_Increment_keyboard_big', 10)
+		Glyphs.registerDefault('com_filipenegrao_Increment_keyboard_huge', 100)
+		
+		Glyphs.registerDefault('com_filipenegrao_Increment_metrics_small', 1)
+		Glyphs.registerDefault('com_filipenegrao_Increment_metrics_big', 10)
 
-		Glyphs.registerDefault('com.filipenegrao.Increment.metrics_small', 1)
-		Glyphs.registerDefault('com.filipenegrao.Increment.metrics_big', 10)
-
-		Glyphs.registerDefault('com.filipenegrao.Increment.kerning_small', 1)
-		Glyphs.registerDefault('com.filipenegrao.Increment.kerning_big', 10)
-
-		Glyphs.registerDefault('com.filipenegrao.Increment.keyboard', False)
-		Glyphs.registerDefault('com.filipenegrao.Increment.metrics', False)
-		Glyphs.registerDefault('com.filipenegrao.Increment.kerning', False)
-
-		self.keyboard_txt_keys.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.keyboard_small'])
-		self.keyboard_txt_shift.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.keyboard_big'])
-
-		self.metrics_txt_keys.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.metrics_small'])
-		self.metrics_txt_shift.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.metrics_big'])
-
-		self.kerning_txt_keys.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.kerning_small'])
-		self.kerning_txt_shift.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.kerning_big'])
-
-		self.keyboard_switch.setObjectValue_(bool(Glyphs.defaults['com.filipenegrao.Increment.keyboard']))
-		self.metrics_switch.setObjectValue_(bool(Glyphs.defaults['com.filipenegrao.Increment.metrics']))
-		self.kerning_switch.setObjectValue_(bool(Glyphs.defaults['com.filipenegrao.Increment.kerning']))
-
-		# if Glyphs.versionNumber <= 3.0:
-		# 	self.keyboard_txt_shift.isEditable = False
-
-	@objc.IBAction
-	def setKeyboardSmall_(self, sender):
-		key_small = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.keyboard_small'] = key_small
-		self.keyboard_txt_keys.setIntValue_(key_small)
-
-	@objc.IBAction
-	def setKeyboardBig_(self, sender):
-		key_big = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.keyboard_big'] = key_big
-		self.keyboard_txt_shift.setIntValue_(key_big)
+		Glyphs.registerDefault('com_filipenegrao_Increment_kerning_small', 1)
+		Glyphs.registerDefault('com_filipenegrao_Increment_kerning_big', 10)
+		print("__keyboard", Glyphs.intDefaults['com_filipenegrao_Increment_keyboard_small'], Glyphs.intDefaults['com_filipenegrao_Increment_keyboard_big'])
 
 	@objc.IBAction
 	def applyKeyboard_(self, sender):
-		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
-
-		if switch_on:
+		if Glyphs.boolDefaults["com_filipenegrao_Increment_keyboard"]:
 			try:
 				# Glyphs 3
-				font.keyboardIncrement = Glyphs.defaults['com.filipenegrao.Increment.keyboard_small']
-				font.keyboardIncrementBig = Glyphs.defaults['com.filipenegrao.Increment.keyboard_big']
+				font.keyboardIncrement = Glyphs.intDefaults['com_filipenegrao_Increment_keyboard_small']
+				font.keyboardIncrementBig = Glyphs.intDefaults['com_filipenegrao_Increment_keyboard_big']
+				font.keyboardIncrementHuge = Glyphs.intDefaults['com_filipenegrao_Increment_keyboard_huge']
 			except:
 				# Glyphs 2
-				font.keyboardIncrement = Glyphs.defaults['com.filipenegrao.Increment.keyboard_small']
-
+				pass
 		else:
 			try:
 				# Glyphs 3
 				font.keyboardIncrement = 1
 				font.keyboardIncrementBig = 10
+				font.keyboardIncrementHuge = 100
 			except:
 				# Glyphs 2
-				font.keyboardIncrement = 1
-
-	@objc.IBAction
-	def setMetricsSmall_(self, sender):
-		metrics_small = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.metrics_small'] = metrics_small
-		self.metrics_txt_keys.setIntValue_(metrics_small)
-
-	@objc.IBAction
-	def setMetricsBig_(self, sender):
-		metrics_big = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.metrics_big'] = metrics_big
-		self.metrics_txt_shift.setIntValue_(metrics_big)
+				pass
 
 	@objc.IBAction
 	def applyMetrics_(self, sender):
-		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
-
-		if switch_on:
-			Glyphs.intDefaults["GSSpacingIncrementLow"] = Glyphs.defaults['com.filipenegrao.Increment.metrics_small']
-			Glyphs.intDefaults["GSSpacingIncrementHigh"] = Glyphs.defaults['com.filipenegrao.Increment.metrics_big']
+		if Glyphs.boolDefaults["com_filipenegrao_Increment_metrics"]:
+			Glyphs.intDefaults["GSSpacingIncrementLow"] = Glyphs.intDefaults['com_filipenegrao_Increment_metrics_small']
+			Glyphs.intDefaults["GSSpacingIncrementHigh"] = Glyphs.intDefaults['com_filipenegrao_Increment_metrics_big']
 		else:
 			Glyphs.intDefaults["GSSpacingIncrementLow"] = 1
 			Glyphs.intDefaults["GSSpacingIncrementHigh"] = 10
 
 	@objc.IBAction
-	def setKerningSmall_(self, sender):
-		kerning_small = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.kerning_small'] = kerning_small
-		self.kerning_txt_keys.setIntValue_(kerning_small)
-
-	@objc.IBAction
-	def setKerningBig_(self, sender):
-		kerning_big = sender.intValue()
-		Glyphs.defaults['com.filipenegrao.Increment.kerning_big'] = kerning_big
-		self.kerning_txt_shift.setIntValue_(kerning_big)
-
-	@objc.IBAction
 	def applyKerning_(self, sender):
-		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
-
-		if switch_on:
-			Glyphs.intDefaults["GSKerningIncrementLow"] = Glyphs.defaults['com.filipenegrao.Increment.kerning_small']
-			Glyphs.intDefaults["GSKerningIncrementHigh"] = Glyphs.defaults['com.filipenegrao.Increment.kerning_big']
+		if Glyphs.boolDefaults["com_filipenegrao_Increment_kerning"]:
+			Glyphs.intDefaults["GSKerningIncrementLow"] = Glyphs.intDefaults['com_filipenegrao_Increment_kerning_small']
+			Glyphs.intDefaults["GSKerningIncrementHigh"] = Glyphs.intDefaults['com_filipenegrao_Increment_kerning_big']
 		else:
 			Glyphs.intDefaults["GSKerningIncrementLow"] = 1
 			Glyphs.intDefaults["GSKerningIncrementHigh"] = 10
