045ea563d37e5b8e1ff1563500dff7a9c5c08cbe
schriftgestalt <georg.seifert@mac.com>
2024-07-25 09:49:57 -0700

    cleanup

--- ChangeIncrement.glyphsPalette/Contents/Resources/plugin.py
+++ ChangeIncrement.glyphsPalette/Contents/Resources/plugin.py
@@ -3,25 +3,26 @@
 ###########################################################################################################
 #
 #
-#	Palette Plugin
+#  Palette Plugin
 #
-#	Read the docs:
-#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
+#  Read the docs:
+#  https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/Palette
 #
 #
 ###########################################################################################################
 
 from __future__ import division, print_function, unicode_literals
 import objc
-from GlyphsApp import *
-from GlyphsApp.plugins import *
+from GlyphsApp import Glyphs
+from GlyphsApp.plugins import PalettePlugin
 import platform
 
+
 class KeyboardIncrement(PalettePlugin):
-	
+
 	# the view
 	dialog = objc.IBOutlet()
-	
+
 	# keyboard increment changer UIs
 	keyboard_switch = objc.IBOutlet()
 	keyboard_txt_keys = objc.IBOutlet()
@@ -45,13 +46,12 @@ class KeyboardIncrement(PalettePlugin):
 			# 'fr': 'Ma palette',
 			# 'es': 'Mi panel',
 			'pt': 'Incremento de Teclado',
-			})
-		
+		})
+
 		# Load .nib dialog (without .extension)
-		version = platform.mac_ver()[0]
 		versionStr = platform.mac_ver()[0]
 		parts = versionStr.split(".")
-		if float(parts[0]+"."+parts[1]) >= 10.15:
+		if float(parts[0] + "." + parts[1]) >= 10.15:
 			self.loadNib('IBdialog', __file__)
 		else:
 			self.loadNib('IBdialogpre15', __file__)
@@ -71,7 +71,6 @@ class KeyboardIncrement(PalettePlugin):
 		Glyphs.registerDefault('com.filipenegrao.Increment.metrics', False)
 		Glyphs.registerDefault('com.filipenegrao.Increment.kerning', False)
 
-
 		self.keyboard_txt_keys.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.keyboard_small'])
 		self.keyboard_txt_shift.setIntValue_(Glyphs.defaults['com.filipenegrao.Increment.keyboard_big'])
 
@@ -89,19 +88,19 @@ class KeyboardIncrement(PalettePlugin):
 		# 	self.keyboard_txt_shift.isEditable = False
 
 	@objc.IBAction
-	def setKeyboardSmall_( self, sender ):
+	def setKeyboardSmall_(self, sender):
 		key_small = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.keyboard_small'] = key_small
 		self.keyboard_txt_keys.setIntValue_(key_small)
 
 	@objc.IBAction
-	def setKeyboardBig_( self, sender ):
+	def setKeyboardBig_(self, sender):
 		key_big = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.keyboard_big'] = key_big
 		self.keyboard_txt_shift.setIntValue_(key_big)
 
 	@objc.IBAction
-	def applyKeyboard_( self, sender):
+	def applyKeyboard_(self, sender):
 		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
 
@@ -124,19 +123,19 @@ class KeyboardIncrement(PalettePlugin):
 				font.keyboardIncrement = 1
 
 	@objc.IBAction
-	def setMetricsSmall_( self, sender ):
-		metrics_small = sender.intValue() 
+	def setMetricsSmall_(self, sender):
+		metrics_small = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.metrics_small'] = metrics_small
 		self.metrics_txt_keys.setIntValue_(metrics_small)
 
 	@objc.IBAction
-	def setMetricsBig_( self, sender ):
-		metrics_big = sender.intValue() 
+	def setMetricsBig_(self, sender):
+		metrics_big = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.metrics_big'] = metrics_big
 		self.metrics_txt_shift.setIntValue_(metrics_big)
 
 	@objc.IBAction
-	def applyMetrics_( self, sender):
+	def applyMetrics_(self, sender):
 		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
 
@@ -147,21 +146,20 @@ class KeyboardIncrement(PalettePlugin):
 			Glyphs.intDefaults["GSSpacingIncrementLow"] = 1
 			Glyphs.intDefaults["GSSpacingIncrementHigh"] = 10
 
-
 	@objc.IBAction
-	def setKerningSmall_( self, sender ):
-		kerning_small = sender.intValue() 
+	def setKerningSmall_(self, sender):
+		kerning_small = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.kerning_small'] = kerning_small
 		self.kerning_txt_keys.setIntValue_(kerning_small)
 
 	@objc.IBAction
-	def setKerningBig_( self, sender ):
-		kerning_big = sender.intValue() 
+	def setKerningBig_(self, sender):
+		kerning_big = sender.intValue()
 		Glyphs.defaults['com.filipenegrao.Increment.kerning_big'] = kerning_big
 		self.kerning_txt_shift.setIntValue_(kerning_big)
 
 	@objc.IBAction
-	def applyKerning_( self, sender):
+	def applyKerning_(self, sender):
 		switch_on = bool(sender.objectValue())
 		font = Glyphs.font
 
@@ -173,22 +171,22 @@ class KeyboardIncrement(PalettePlugin):
 			Glyphs.intDefaults["GSKerningIncrementHigh"] = 10
 
 	# @objc.IBAction
-	# def setMetrics_( self, sender ):
+	# def setMetrics_(self, sender):
 	# 	# print('Metrics')
 	# 	Glyphs.defaults['com.filipenegrao.KeyboardIncrement.metrics'] = bool(sender.objectValue())
 
 	# @objc.IBAction
-	# def setKerning_( self, sender ):
+	# def setKerning_(self, sender):
 	# 	# print('Kerning')
 	# 	Glyphs.defaults['com.filipenegrao.KeyboardIncrement.kerning'] = bool(sender.objectValue())
 
 	# @objc.IBAction
-	# def setKeyboard_( self, sender ):
+	# def setKeyboard_(self, sender):
 	# 	# print('Keyboard')
 	# 	Glyphs.defaults['com.filipenegrao.KeyboardIncrement.keyboard'] = bool(sender.objectValue())
 
 	# @objc.IBAction
-	# def applyButton_( self, sender ):
+	# def applyButton_(self, sender):
 
 	# 	font = Glyphs.font
 
@@ -210,13 +208,12 @@ class KeyboardIncrement(PalettePlugin):
 	# 		font.keyboardIncrement = incValue
 	# 		font.keyboardIncrementBig = incValue * 10
 
-
 	# @objc.IBAction
-	# def resetButton_( self, sender ):
+	# def resetButton_(self, sender):
 	# 	print('Reset Button')
 
 	# @objc.python_method
-	# def updateControls( self, sender = None, ui_field ):
+	# def updateControls(self, sender = None, ui_field):
 	# 	# self.displayValue.setStringValue_(str(Glyphs.defaults['com.filipenegrao.KeyboardIncrement.increment']))
 	# 	self.stepper.setIntValue_(Glyphs.defaults['com.filipenegrao.KeyboardIncrement.increment'])
 	# 	self.displayValue.setIntValue_(Glyphs.defaults['com.filipenegrao.KeyboardIncrement.increment'])
@@ -229,4 +226,3 @@ class KeyboardIncrement(PalettePlugin):
 	def __file__(self):
 		"""Please leave this method unchanged"""
 		return __file__
-
